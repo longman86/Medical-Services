4e56e5e7fd77e88442d36a068898c28330b5d6c8
longman86 <ademola.atolagbe16@gmail.com>
2023-01-12 17:04:18 +0100

    final

--- WellnessApp.py
+++ WellnessApp.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import requests
+import io
 from PIL import Image
 
 st.set_page_config(layout='wide')
@@ -8,8 +10,12 @@ st.image(image, use_column_width=False)
 st.title('AVON HMO Wellness Survey')
 
 
-path = r'C:\Users\ademola.atolagbe\OneDrive - Avon Healthcare Ltd\Member Wellness.csv'
-wellness_df = pd.read_csv(path)
+url = "https://raw.githubusercontent.com/longman86/Avon-Wellness-Survey/main/Member_Wellness.csv" # Make sure the url is the raw version of the file on GitHub
+download = requests.get(url).content
+wellness_df = pd.read_csv(io.StringIO(download.decode('utf-8')))
+#path = r'C:\Users\ademola.atolagbe\Documents\Medical_Services\Member_Wellness.csv'
+#path = 'https://avonhealthcareltd-my.sharepoint.com/personal/droguguah_ejinkeonye_avonhealthcare_com/Documents/Documents/Wellness%20member%20list.xlsx?web=1'
+#wellness_df = pd.read_csv(path)
 
 
 enrollee_id = st.text_input('Kindly input your member number and press the enter key to confirm your eligibility')
@@ -22,7 +28,7 @@ if enrollee_id:
     if enrollee_id in wellness_df['AvonoldEnrolleId'].values:
         enrollee_name = wellness_df.loc[wellness_df['AvonoldEnrolleId'] == enrollee_id, 'membername'].values[0]
         st.balloons()
-        st.info(f'Welcome {enrollee_name} \n Please click the link below to continue',icon="✅")
+        st.info(f'Welcome {enrollee_name}. \n Please click the link below to continue',icon="✅")
         st.write("[AVON Wellness Survey](https://forms.office.com/Pages/ResponsePage.aspx?id=y7xkPyIn5UK1ynGyBgRdh8-wGEk3Z51JtzuJQUW5THtUN0s5RVJQOFQ5R0swSktXSlQyWlBBUjFWUi4u&origin=Invitation&channel=1)")
        
     elif enrollee_id not in wellness_df['AvonoldEnrolleId'].values:
