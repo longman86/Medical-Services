7c0d37d58ca202bf806a44678d724b233fbe962d
longman86 <ademola.atolagbe16@gmail.com>
2023-01-17 15:48:11 +0100

    updated the source file

--- WellnessApp.py
+++ WellnessApp.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import requests
-import io
 from PIL import Image
 
 st.set_page_config(layout='wide')
@@ -10,28 +8,29 @@ st.image(image, use_column_width=False)
 st.title('AVON HMO Wellness Survey')
 
 
-url = "https://raw.githubusercontent.com/longman86/Avon-Wellness-Survey/main/Member_Wellness.csv" # Make sure the url is the raw version of the file on GitHub
-download = requests.get(url).content
-wellness_df = pd.read_csv(io.StringIO(download.decode('utf-8')))
+#url = "https://raw.githubusercontent.com/longman86/Avon-Wellness-Survey/main/Member_Wellness.csv" # Make sure the url is the raw version of the file on GitHub
+#download = requests.get(url).content
+#wellness_df = pd.read_csv(io.StringIO(download.decode('utf-8')))
 #path = r'C:\Users\ademola.atolagbe\Documents\Medical_Services\Member_Wellness.csv'
 #path = 'https://avonhealthcareltd-my.sharepoint.com/personal/droguguah_ejinkeonye_avonhealthcare_com/Documents/Documents/Wellness%20member%20list.xlsx?web=1'
-#wellness_df = pd.read_csv(path)
+wellness_df = pd.read_csv('Wellness_Member_List.csv')
+wellness_df['MemberNo'] = wellness_df['MemberNo'].astype(str)
 
 
 enrollee_id = st.text_input('Kindly input your member number and press the enter key to confirm your eligibility')
-enrollee_id = enrollee_id.upper()
+enrollee_id = str(enrollee_id)
 
 
 #def IsMemberEligible(enrollee_id):
 #member_list = wellness_df.AvonoldEnrolleId.tolist()
 if enrollee_id:
-    if enrollee_id in wellness_df['AvonoldEnrolleId'].values:
-        enrollee_name = wellness_df.loc[wellness_df['AvonoldEnrolleId'] == enrollee_id, 'membername'].values[0]
+    if enrollee_id in wellness_df['MemberNo'].values:
+        enrollee_name = wellness_df.loc[wellness_df['MemberNo'] == enrollee_id, 'membername'].values[0]
         st.balloons()
         st.info(f'Welcome {enrollee_name}. \n Please click the link below to continue',icon="✅")
         st.write("[AVON Wellness Survey](https://forms.office.com/Pages/ResponsePage.aspx?id=y7xkPyIn5UK1ynGyBgRdh8-wGEk3Z51JtzuJQUW5THtUN0s5RVJQOFQ5R0swSktXSlQyWlBBUjFWUi4u&origin=Invitation&channel=1)")
        
-    elif enrollee_id not in wellness_df['AvonoldEnrolleId'].values:
+    elif enrollee_id not in wellness_df['MemberNo'].values:
         st.info('You are not eligible to participate, please contact your HR or Client Manager')
 else:
     st.write('You must input your Member number to continue')
