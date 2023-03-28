22a978df15ec59d370b4e83fc6c31982bedd58b5
longman86 <ademola.atolagbe16@gmail.com>
2023-03-28 10:19:56 +0100

    added files to gitignore

--- WellnessApp.py
+++ WellnessApp.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import datetime as dt
+import pyodbc
 
 st.set_page_config(layout='wide')
 image = Image.open("Pre-ScreeningHeader.png")
@@ -8,13 +10,25 @@ st.image(image, use_column_width=True)
 st.title('AVON Pre Screening Medical Assessment Portal')
 
 
-#url = "https://raw.githubusercontent.com/longman86/Avon-Wellness-Survey/main/Member_Wellness.csv" # Make sure the url is the raw version of the file on GitHub
-#download = requests.get(url).content
-#wellness_df = pd.read_csv(io.StringIO(download.decode('utf-8')))
-#path = r'C:\Users\ademola.atolagbe\Documents\Medical_Services\Member_Wellness.csv'
-#path = 'https://avonhealthcareltd-my.sharepoint.com/personal/droguguah_ejinkeonye_avonhealthcare_com/Documents/Documents/Wellness%20member%20list.xlsx?web=1'
-wellness_df = pd.read_csv('Wellness_Member_List.csv')
-wellness_df['MemberNo'] = wellness_df['MemberNo'].astype(str)
+@st.experimental_memo(ttl = dt.timedelta(hours=24))
+def get_data_from_sql(query):
+    conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 18 for SQL Server};SERVER='
+        +st.secrets['server']
+        +';DATABASE='
+        +st.secrets['database']
+        +';UID='
+        +st.secrets['username']
+        +';PWD='
+        +st.secrets['password']
+        )
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
+query = 'SELECT * from vw_wellness_enrollee'
+wellness_df = get_data_from_sql(query=query)
+#wellness_df = pd.read_csv('Wellness_Member_List.csv')
+wellness_df['memberno'] = wellness_df['memberno'].astype(str)
 
 
 enrollee_id = st.text_input('Kindly input your member number and press the enter key to confirm your eligibility')
@@ -24,13 +38,13 @@ enrollee_id = str(enrollee_id)
 #def IsMemberEligible(enrollee_id):
 #member_list = wellness_df.AvonoldEnrolleId.tolist()
 if enrollee_id:
-    if enrollee_id in wellness_df['MemberNo'].values:
-        enrollee_name = wellness_df.loc[wellness_df['MemberNo'] == enrollee_id, 'membername'].values[0]
+    if enrollee_id in wellness_df['memberno'].values:
+        enrollee_name = wellness_df.loc[wellness_df['memberno'] == enrollee_id, 'membername'].values[0]
         st.balloons()
         st.info(f'Welcome {enrollee_name}.\n \n By clicking the link below, I understand and hereby acknowledge that my data would be collected and processed only for the performance of this wellness screening exercise.',icon="✅")
         st.write("[AVON Wellness Survey](https://forms.office.com/Pages/ResponsePage.aspx?id=y7xkPyIn5UK1ynGyBgRdhwThIjvb_VxNtMX-P8ytR_1UQ01ZRU05WFhERkRIQTVLRlZVUTNNSTcxMS4u)")
        
-    elif enrollee_id not in wellness_df['MemberNo'].values:
+    elif enrollee_id not in wellness_df['memberno'].values:
         st.info('You are not eligible to participate, please contact your HR or Client Manager')
 else:
     st.write('You must input your Member number to continue')
